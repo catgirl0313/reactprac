462d3d970ea793322361709d73f0439a49f26dbf
Jiyoung Baek <vvvv4449@gmail.com>
2022-12-13 01:33:55 +0900

    week2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Start from "./Start";
 
 function App() {
+  const inputRef=React.useRef() 
+  const [name, setName]=React.useState("")
+
+
+  const handleClick=()=>{
+    setName(inputRef.current.value)
+    console.log(inputRef.current.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Start name={name}></Start>
+      <input ref={inputRef} placeholder="내 이름"></input>//매핑
+      <button onClick={handleClick}>시작하기</button>
+      
+      
+     
     </div>
   );
 }
---
+++ src/Start.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Start = (props) => {
+  const name=props.name
+  
+  return (
+    <div>나는 {name}에 대해서 얼마나 알고있을까?</div>
+  )
+}
+
+export default Start
